bedd507c96569b661671caf97117e32e029aa024
Dennis Mwebia <dennisgitonga139@gmail.com>
2022-02-03 07:36:28 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Redirecting</title>
+</head>
+
+    <body>
+        <p>Zoom Login Successful...</p>
+        <!--<a href="com.penguinstech.zoommeetingapp://penguinstech.com/?code=1234">sdfsd</a>-->
+        <a id="link" href="intent://penguinstech.com/?code=1234#Intent;scheme=com.penguinstech.zoommeetingapp;package=com.penguinstech.zoommeetingapp;end">Continue to App</a>
+    </body>
+    <script>
+        // window.location.replace("com.penguinstech.zoommeetingapp://12345")
+        var url = new URL(window.location);
+        var code = url.searchParams.get("code");
+        var url = "intent://penguinstech.com/?code="+code+"#Intent;scheme=com.penguinstech.zoommeetingapp;package=com.penguinstech.zoommeetingapp;end"
+        document.getElementById('link').href = url
+        // document.getElementById('link').click()
+
+        
+    </script>
+</html>
